506ba4088166cc1a813fbe6290d9c61a4d2dd8f0
sunmin-hwang <tommy08190@naver.com>
2017-06-11 11:17:36 +0900

    print 1~9

--- multiplying/Program.cs
+++ multiplying/Program.cs
@@ -10,6 +10,15 @@ namespace multiplying
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(1);
+            Console.WriteLine(2);
+            Console.WriteLine(3);
+            Console.WriteLine(4);
+            Console.WriteLine(5);
+            Console.WriteLine(6);
+            Console.WriteLine(7);
+            Console.WriteLine(8);
+            Console.WriteLine(9);
         }
     }
 }
