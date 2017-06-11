04af206aec21188d6183fce044491bb75a76c01e
sunmin-hwang <tommy08190@naver.com>
2017-06-11 11:23:31 +0900

    use for

--- multiplying/Program.cs
+++ multiplying/Program.cs
@@ -10,15 +10,10 @@ namespace multiplying
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(1);
-            Console.WriteLine(2);
-            Console.WriteLine(3);
-            Console.WriteLine(4);
-            Console.WriteLine(5);
-            Console.WriteLine(6);
-            Console.WriteLine(7);
-            Console.WriteLine(8);
-            Console.WriteLine(9);
+            for (int i = 1; i <= 9; i++)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
